142b72d1e936d00603c08c8819541a0e1b8d8313
root <root@ip-172-31-30-0.ec2.internal>
2017-10-19 19:50:04 +0000

    changes

--- db/config_db.sh
+++ db/config_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp /opt/db/mongod.conf /etc/mongod.conf.orig && /usr/bin/mongod && mongodump --out /backups/`date +"%m-%d-%y"` && find /backups -mtime +7 -exec rm -rf {} 
+cp /opt/db/mongod.conf /etc/mongod.conf.orig && /usr/bin/mongod && find /backups -mtime +7 -exec rm -rf {} && mongodump --out /backups/`date +"%m-%d-%y"` 
--- scripts/refresh_server.sh
+++ scripts/refresh_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker-compose stop && sudo docker-compose rm -f && sudo docker rmi $(docker images| grep blog | awk '{print $3}') && sudo docker-compose up
+(sudo docker-compose stop && sudo docker-compose rm -f) || (sudo docker rm $(docker ps -a | grep -v 'CONTAINER' |awk '{print $1}')) 
+sudo docker rmi $(docker images| grep -v 'Repository' | awk '{print $3}')
+sudo docker-compose up;
