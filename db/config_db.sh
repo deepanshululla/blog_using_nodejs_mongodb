a9ba545e31e3a45013f03dfb8f3499f24486cec3
Ubuntu <ubuntu@ip-172-31-30-0.ec2.internal>
2017-10-19 23:32:47 +0000

    db backup

--- db/config_db.sh
+++ db/config_db.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cp /opt/db/mongod.conf /etc/mongod.conf.orig && /usr/bin/mongod && find /backups -mtime +7 -exec rm -rf {} && mongodump --out /backups/`date +"%m-%d-%y"` 
+cp /opt/db/mongod.conf /etc/mongod.conf.orig && /usr/bin/mongod
+echo "Starting Server..."
+find /backups -mtime +7 -exec rm -rf {} \;
+echo "Removing older backups"
+mongodump --out /backups/`date +"%m-%d-%y"`
+echo "Backed up DB"
